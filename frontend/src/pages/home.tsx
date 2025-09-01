2c6e6bd431c2e5b663113834629ad6025f00b371
unknown <okereebube87@gmail.com>
2025-09-01 11:30:04 +0100

    ...

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import { NavigationBar } from "../components/navbar";
+import { MainSection } from "../components/mainsection";
+import { FooterSection } from "../components/footer";
+
+export function Home () {
+
+    return (
+        <>
+            <NavigationBar/>
+            <MainSection/>
+            <FooterSection/>
+        </>
+    )
+}
